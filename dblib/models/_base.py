08096db5a7cf8e57f1e1a7677a5cd534409f6c12
Trent Dierking <deerxlord@gmail.com>
2023-01-07 01:02:09 -0600

    updates

--- dblib/models/_base.py
+++ dblib/models/_base.py
@@ -5,3 +5,9 @@ from ..types import TABLE_ID
 
 class Table(SQLModel):
     id: TABLE_ID | None = Field(default=None, primary_key=True)
+
+    @property
+    def __tablename__(self) -> str:
+        *_, module = self.__module__.split(".")
+        name = self.__class__.__name__.lower()
+        return f"{module}_{name}"
