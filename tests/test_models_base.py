22b9314820f9d0ee3feccc02de443ec26e704edd
Trent Dierking <deerxlord@gmail.com>
2023-01-08 03:24:11 -0600

    updates

--- tests/test_models_base.py
+++ tests/test_models_base.py
@@ -1,6 +1,3 @@
-import pytest
-
-from dblib import _create_tables, database
 from dblib.models._base import Table
 
 from . import setup
@@ -11,19 +8,3 @@ def test_tablename():
         ...
 
     assert TestModel.__tablename__ == "test_models_base_testmodel"
-
-
-@pytest.mark.asyncio
-async def test_database(setup):
-    class TestModel(Table, table=True):
-        ...
-
-    model = TestModel()
-
-    await _create_tables()
-    async with database.connection() as db:
-        db.add(model)
-        await db.commit()
-        await db.refresh(model)
-
-    assert model.id == 1
